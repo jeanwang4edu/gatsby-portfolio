4c767d916e0780e6336ffb431af6091b5531306c
Jean Wang <jeanharmsen@yahoo.com>
2020-03-06 10:21:39 -0500

    modify options for source-wordpress plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,31 +62,6 @@ module.exports = {
         // options page using the provided `acf_add_options_page` method, in your WordPress setup
         // Dashes in IDs will be converted to underscores for use in GraphQL
         acfOptionPageIds: [],
-        auth: {
-          // If auth.user and auth.pass are filled, then the source plugin will be allowed
-          // to access endpoints that are protected with .htaccess.
-          htaccess_user: "your-htaccess-username",
-          htaccess_pass: "your-htaccess-password",
-          htaccess_sendImmediately: false,
-
-          // If hostingWPCOM is true then you will need to communicate with wordpress.com API
-          // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
-          // then add your clientId, clientSecret, username, and password here
-          // Learn about environment variables: https://www.gatsbyjs.org/docs/environment-variables
-          // If two-factor authentication is enabled then you need to create an Application-Specific Password,
-          // see https://en.support.wordpress.com/security/two-step-authentication/#application-specific-passwords
-          wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
-          wpcom_app_clientId: "54793",
-          wpcom_user: "gatsbyjswpexample@gmail.com",
-          wpcom_pass: process.env.WORDPRESS_PASSWORD,
-
-          // If you use "JWT Authentication for WP REST API" (https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/)
-          // or (https://github.com/jonathan-dejong/simple-jwt-authentication) requires jwt_base_path, path can be found in WordPress wp-api.
-          // plugin, you can specify user and password to obtain access token and use authenticated requests against WordPress REST API.
-          jwt_user: process.env.JWT_USER,
-          jwt_pass: process.env.JWT_PASSWORD,
-          jwt_base_path: "/jwt-auth/v1/token", // Default - can skip if you are using https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/
-        },
         // Set cookies that should be send with requests to WordPress as key value pairs
         cookies: {},
         // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
@@ -118,37 +93,15 @@ module.exports = {
           "**/tags",
           "**/taxonomies",
           "**/users",
+          "**/menus",
         ],
         // Blacklisted routes using glob patterns
-        excludedRoutes: ["**/posts/1456"],
+        excludedRoutes: [],
         // Set this to keep media sizes.
         // This option is particularly useful in case you need access to
         // URLs for thumbnails, or any other media detail.
         // Defaults to false
         keepMediaSizes: false,
-        // use a custom normalizer which is applied after the built-in ones.
-        normalizer: function({ entities }) {
-          return entities
-        },
-        // The normalizers option allows you to manipulate the array of internal
-        // normalizers that are applied to entities after they're fetched
-        // from WordPress.
-        // You can add your own normalizers to this array by adding an object
-        // that contains name and normalizer properties.
-        // Name is the name of your normalizer, and normalizer is a function that
-        // should return the array of entities that are passed to it.
-        // This is useful if you need more control over the order of normalizers,
-        // instead of your normalizer being applied after the built in normalizers (as is the case with the normalizer option).
-        normalizers: normalizers => [
-          ...normalizers,
-          {
-            name: "nameOfTheFunction",
-            normalizer: function({ entities }) {
-              // manipulate entities here
-              return entities
-            },
-          },
-        ],
       },
     },
   ],
